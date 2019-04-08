f3fa1e05d9641a614aaa315491749cdaec65c6dc
Joe Chandler <jc6882@na.corp>
2019-04-08 16:26:23 -0500

    create function to perform the software check

--- software_check.ps1
+++ software_check.ps1
@@ -1,1 +1,6 @@
-﻿Write-Host("Hello, world!")
+﻿function Software_Check($computer, $programName ) {
+$wmi_check = (Get-WMIObject -ComputerName $computer -Query "SELECT * FROM Win32_Product Where Name Like '%$programName%'").Length -gt 0
+return $wmi_check;
+}
+ 
+Check_Program_Installed("COAC6S8K32","LastPass")
