27633729b726ca0c1a18967b9fcaaf5960fb8e4d
Joe Chandler <jc6882@na.corp>
2019-04-08 17:00:44 -0500

    add simple loop to perform checks on multiple computers

--- software_check.ps1
+++ software_check.ps1
@@ -1,4 +1,12 @@
 ﻿#Specify the program for which we are checking
 $programName = "LastPass"
 
-((Get-WMIObject -Query "SELECT * FROM Win32_Product Where Name Like '%$programName%'").Name).Length -ne 0
+#DEBUG
+#TODO replace with read-in values for computers in the domain
+$Computers = "COAC6S8K32", "COA26SQHQ2", "COA26SSHQ2"
+
+foreach($Computer in $Computers) {
+    Write-Host($Computer)
+}
+
+#((Get-WMIObject -Query "SELECT * FROM Win32_Product Where Name Like '%$programName%'").Name).Length -ne 0
