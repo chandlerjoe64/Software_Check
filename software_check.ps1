d64409b996353c3d82a7ea40eb143f9ba031285b
Joe Chandler <jc6882@na.corp>
2019-04-08 16:57:11 -0500

    change from function to one-liner

--- software_check.ps1
+++ software_check.ps1
@@ -1,21 +1,4 @@
-﻿#Function originally copied from https://www.morgantechspace.com/2018/02/check-if-software-program-is-installed-powershell.html
+﻿#Specify the program for which we are checking
+$programName = "LastPass"
 
-function Software_Check($computer, $programName ) {
-    #Use get-wmi to check if remote device has software installed, suppressing error messages, and returning a boolean value
-    $wmi_check = (Get-WMIObject -erroraction 'silentlycontinue' -ComputerName $computer -Query "SELECT * FROM Win32_Product Where Name Like '%$programName%'").Length -gt 0
-    return $wmi_check; #Boolean value
-}
- 
-#This is a check for software on the local machine for software that is KNOWN INSTALLED
-if(Software_Check("COAC6S8K32","LastPass")) {
-    Write-Host("LastPass is installed")
-} else {
-    Write-Host("LastPass is NOT installed")
-}
-
-#This is a check for software on the local machine for software that is NOT INSTALLED
-if(Software_Check("COAC6S8K32","Safari")) {
-    Write-Host("Safari is installed")
-} else {
-    Write-Host("Safari is NOT installed")
-}
+((Get-WMIObject -Query "SELECT * FROM Win32_Product Where Name Like '%$programName%'").Name).Length -ne 0
